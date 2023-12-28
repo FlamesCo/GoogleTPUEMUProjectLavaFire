e7093914723dc8d077d3cb33cca4d2647879e82e
Flames Co. LLC <84363801+FlamesCo@users.noreply.github.com>
2023-12-27 16:39:58 -0800

    Add files via upload

--- GoogleTPUEMUV0.sh
+++ GoogleTPUEMUV0.sh
@@ -22,7 +22,7 @@ transform_m1_to_tpu() {
 # CLI for user to select optimization options
 cli() {
   echo 'Select an optimization option:'
-  select opt in 'VRAM' 'GPU' 'Metal API' 'All'; do
+  select opt in 'VRAM' 'GPU' 'Metal API' 'Transform M1 to TPU' 'All'; do
     case $opt in
       'VRAM')
         optimize_vram
@@ -34,11 +34,15 @@ cli() {
         echo 'Optimizing Metal API...'
         # Insert Metal API optimization commands here
         ;;
+      'Transform M1 to TPU')
+        transform_m1_to_tpu
+        ;;
       'All')
         optimize_vram
         optimize_gpu
         echo 'Optimizing Metal API...'
         # Insert Metal API optimization commands here
+        transform_m1_to_tpu
         ;;
       *)
         echo 'Invalid option. Please try again.'
